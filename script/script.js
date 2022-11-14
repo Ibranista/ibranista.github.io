6b16bb518cc7fef9ab9e2a35215e9d12e8ebab5a
Ibranista <ibranista88@gmail.com>
2022-11-14 13:22:07 +0100

    added toggler for the active and hide class.

--- script/script.js
+++ script/script.js
@@ -1,10 +1,9 @@
-let Menu1 = document.querySelector("#menu-1");
-let Menu2 = document.querySelector("#menu-2");
-let Menu3 = document.querySelector("#menu-3");
-let humMenu = document.getElementById("hum-menu");
-const Toggler = (e) => {
-  e.preventDefault();
-  
+const humMenu = document.querySelector(".hum-menu");
+const navWrapper = document.querySelector(".nav-wrapper");
+const sideNav = document.querySelector(".side-nav");
+const Toggler = () => {
+  navWrapper.classList.toggle("active");
+  sideNav.classList.toggle("hide");
 };
 
 humMenu.addEventListener("click", Toggler);
