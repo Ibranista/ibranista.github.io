a2efbfed4a77165eeb57cde65c25dc18d9b6990e
Ibranista <ibranista88@gmail.com>
2022-11-14 11:36:36 +0100

    imported javascript.

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let Menu1 = document.querySelector("#menu-1");
+let Menu2 = document.querySelector("#menu-2");
+let Menu3 = document.querySelector("#menu-3");
+let humMenu = document.getElementById("hum-menu");
+const Toggler = (e) => {
+  e.preventDefault();
+  
+};
+
+humMenu.addEventListener("click", Toggler);
