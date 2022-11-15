f6cf7f5cafe0ec33452163071348efda33cedb22
Ibranista <ibranista88@gmail.com>
2022-11-15 05:19:07 +0100

    Added a functionality to toggle the menu when links are clicked.

--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
 const humMenu = document.querySelector(".hum-menu");
 const navWrapper = document.querySelector(".nav-wrapper");
 const sideNav = document.querySelector(".side-nav");
+const navLinks = document.querySelectorAll(".nav-link");
+console.log(navLinks);
 const Toggler = (e) => {
   e.preventDefault();
   navWrapper.classList.toggle("active");
   sideNav.classList.toggle("hide");
+  console.log("first");
 };
 
 humMenu.addEventListener("click", Toggler);
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", () => sideNav.classList.add("hide"));
+}
