c87b30dfdec778575105a283721236a177ebeed8
Ibranista <ibranista88@gmail.com>
2022-11-16 18:48:22 +0100

    completed form validation.

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const humMenu = document.querySelector(".hum-menu");
 const navWrapper = document.querySelector(".nav-wrapper");
 const sideNav = document.querySelector(".side-nav");
 const navLinks = document.querySelectorAll(".nav-link");
-console.log(navLinks);
+let message = document.querySelector(".ib");
 const Toggler = (e) => {
   e.preventDefault();
   navWrapper.classList.toggle("active");
@@ -18,3 +18,32 @@ for (let i = 0; i < navLinks.length; i++) {
     navWrapper.classList.remove("active");
   });
 }
+
+// email validation
+const userEmail = document.querySelector("#user-email");
+const formButton = document.querySelector(".btn-get");
+const form = document.getElementById("getForm");
+const email = form.elements["email"];
+const emailRegex = "^[a-z0-9_-]+$";
+
+// Check email is valid
+function checkEmail(input) {
+  const re = /^[a-z]+$/g;
+
+  if (re.test(input)) {
+    message.innerHTML = "";
+    form.submit()
+  } else {
+    message.innerHTML =
+      " email address should be written in small letters only!";
+  }
+}
+
+// event listener
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let Email = email.value.replace(/[^a-zA-Z0-9 ]/g, "");
+  Email=Email.replace(/[0-9]/g, "");
+  checkEmail(Email);
+});
