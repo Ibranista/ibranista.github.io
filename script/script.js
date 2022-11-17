7476e10a12cb82d8db4b5d5e023049e7c7fae835
Ibranista <ibranista88@gmail.com>
2022-11-17 19:41:43 +0100

    fixed linter issues.

--- script/script.js
+++ script/script.js
@@ -1,75 +1,70 @@
-const humMenu = document.querySelector(".hum-menu");
-const navWrapper = document.querySelector(".nav-wrapper");
-const sideNav = document.querySelector(".side-nav");
-const navLinks = document.querySelectorAll(".nav-link");
-let message = document.querySelector(".error");
+/* eslint-disable linebreak-style */
+const humMenu = document.querySelector('.hum-menu');
+const navWrapper = document.querySelector('.nav-wrapper');
+const sideNav = document.querySelector('.side-nav');
+const navLinks = document.querySelectorAll('.nav-link');
+const message = document.querySelector('.error');
 const Toggler = (e) => {
   e.preventDefault();
-  navWrapper.classList.toggle("active");
-  sideNav.classList.toggle("hide");
-  console.log("first");
+  navWrapper.classList.toggle('active');
+  sideNav.classList.toggle('hide');
 };
 
-humMenu.addEventListener("click", Toggler);
+humMenu.addEventListener('click', Toggler);
 
-for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click", () => {
-    sideNav.classList.add("hide");
-    navWrapper.classList.remove("active");
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', () => {
+    sideNav.classList.add('hide');
+    navWrapper.classList.remove('active');
   });
 }
 
 // email validation
-const userEmail = document.querySelector("#user-email");
-const formButton = document.querySelector(".btn-get");
-const form = document.getElementById("getForm");
-const email = form.elements["email"];
-const emailRegex = "^[a-z0-9_-]+$";
-const Name = document.querySelector(".Name");
-const fullName = form.elements["fullname"];
-const textArea = document.querySelector(".textArea");
-const textarea = form.elements["comment"];
+const userEmail = document.querySelector('#user-email');
+const form = document.getElementById('getForm');
+const { email } = form.elements;
+const Name = document.querySelector('.Name');
+const fullName = form.elements.fullname;
+const textArea = document.querySelector('.textArea');
+const textarea = form.elements.comment;
 // Check email is valid
 function checkEmail(input) {
   const re = /^[a-z]+$/g;
 
   if (re.test(input)) {
-    message.innerHTML = "";
+    message.innerHTML = '';
     form.submit();
   } else {
-    message.innerHTML =
-      " email address should be written in small letters only!";
+    message.innerHTML = ' email address should be written in small letters only!';
   }
 }
 
 // event listener
 
-form.addEventListener("submit", function (e) {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let Email = email.value.replace(/[^a-zA-Z0-9 ]/g, "");
-  Email = Email.replace(/[0-9]/g, "");
+  let Email = email.value.replace(/[^a-zA-Z0-9 ]/g, '');
+  Email = Email.replace(/[0-9]/g, '');
   checkEmail(Email);
 });
 
 const onChange = (e) => {
   e.preventDefault();
-  localStorage.setItem("fname", fullName.value);
-  localStorage.setItem("comment", textarea.value);
-  localStorage.setItem("email", email.value);
+  localStorage.setItem('fname', fullName.value);
+  localStorage.setItem('comment', textarea.value);
+  localStorage.setItem('email', email.value);
 };
 
-let fName = localStorage.getItem("fname");
-let Comment = localStorage.getItem("comment");
-let Email = localStorage.getItem("email");
+const fName = localStorage.getItem('fname');
+const Comment = localStorage.getItem('comment');
+const Email = localStorage.getItem('email');
 
 if (Name || Comment || Email) {
   fullName.value = fName;
   textarea.value = Comment;
   email.value = Email;
-} else {
-  console.log("no values yet!");
 }
 
-Name.addEventListener("change", onChange);
-textArea.addEventListener("change", onChange);
-userEmail.addEventListener("change", onChange);
+Name.addEventListener('change', onChange);
+textArea.addEventListener('change', onChange);
+userEmail.addEventListener('change', onChange);
